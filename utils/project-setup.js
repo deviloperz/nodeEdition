2fbc6984c67c14c356624651f86504c2b298d081
deviloperz | Marvin Ernst <deviloperz@gmail.com>
2015-02-06 14:05:38 +0100

    Added data to the template files

--- utils/project-setup.js
+++ utils/project-setup.js
@@ -7,10 +7,9 @@ var setupProject = function () {
   var dbConfig = {};
   var connection;
   
-  var templates = [];
   var folders = [];
 
-  var init, getFoldersFromDb, createTempalteFolders, createTempalteFiles;
+  var init, getFoldersFromDb, createTempalteFolders, createTempalteFiles, save;
 
   init = function (config, callback) {
     var _callback = callback;
@@ -50,7 +49,11 @@ var setupProject = function () {
   };
 
   createTempalteFiles = function (callback) {
-    var _callback = callback;
+    var _callback = callback, getDataId, writeFiles, getData;
+    var templates = [];
+    var contentIds = [];
+    var counter = 0;
+
     connection = mysql.createConnection(dbConfig);
     connection.connect();
     connection.query('SELECT * FROM tbltemplates WHERE IsFolder = 0 ', function(err, rows, fields) {
@@ -60,13 +63,56 @@ var setupProject = function () {
 
         for (var i in rows) {
           templates.push(rows[i]);
-          if (!fs.existsSync('app' + rows[i].Path)) {
-            fs.writeFileSync('app' + rows[i].Path, '');
-          }
         }
-        _callback();
+        getDataId();
     });
-    connection.end();
+
+    getDataId = function () {
+      var templateId = templates[counter].ID, 
+      query = 'SELECT CID FROM tbllink WHERE DID = ' + templateId + ' AND Name = "data" AND DocumentTable = "tblTemplates"';
+
+      connection.query(query, function(err, rows, fields) {
+        contentIds.push(rows[0].CID);
+        if (counter < templates.length - 1) {
+          counter++;
+          getDataId();
+        } else {
+          console.log(contentIds);
+          counter = 0;
+          getData();
+        }
+      });
+    };
+    
+    getData = function () {
+
+      var contentId = contentIds[counter],
+          query = 'SELECT Dat FROM tblcontent WHERE ID = ' + contentId;
+
+      connection.query(query, function(err, rows, fields) {
+
+        templates[counter].data = rows[0].Dat;
+        if (counter < templates.length - 1) {
+          counter++;
+          getData();
+        } else {
+          connection.end();
+          writeFiles();
+        }
+      });
+    };
+
+    writeFiles = function () {
+      for (var i in templates) {
+        if (!fs.existsSync('app' + templates[i].Path)) {
+          fs.writeFileSync('app' + templates[i].Path, templates[i].data);
+        }
+      }
+      _callback();
+    };
+  };
+
+  save = function () {
 
   };
 
